f8de05b5e377d34d45b1e8b29ddbf40aa2493a81
Olivera Kwok <okwokyk@gmail.com>
2019-05-17 11:19:44 +0100

    reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @reivew = Review.find(params[:id])
+    @reivew = Review.new
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    render 'items'
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    render 'items'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :user_id)
+  end
+
 end
