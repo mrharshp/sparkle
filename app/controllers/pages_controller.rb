07f07e84da1afcc63cbc6d93fc7cf89cd615b43c
Hussein Daher <hussein.daher.24@gmail.com>
2019-05-17 11:07:53 +0100

    updared navbar, added my profile/my purchases/my posts pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def profile
+    @user = current_user
+  end
+
+  def purchases
+    @user = current_user
+  end
+
+  def posts
+    @user = current_user
+  end
 end
