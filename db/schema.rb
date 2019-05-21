5f3b43b931e05738d595e3af5bed5b5a69d004b4
Harsh Patel <harshp0301@gmail.com>
2019-05-21 14:44:25 +0100

    Rating column added to Reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_094745) do
+ActiveRecord::Schema.define(version: 2019_05_21_134300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_094745) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "rating"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
